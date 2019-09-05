a3de26d0804f22e7c63b411fde165b5902226f0f
Student <example@example.com>
2019-09-05 16:19:28 -0700

    Show day return at input field.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+const convertToDay = (date) => {
+  const year = date[0];
+  const month = date[1];
+  const day = date[2];
+  const getDate = new Date(year, month, day);
+  const weekday = new Array(7);
+  weekday[0] = "Saturday";
+  weekday[1] = "Sunday";
+  weekday[2] = "Monday";
+  weekday[3] = "Tuesday";
+  weekday[4] = "Wednesday";
+  weekday[5] = "Thursday";
+  weekday[6] = "Friday";
+  
+  const wd = weekday[getDate.getDate()];
+  return wd;
+}
+
 $(document).ready(function(){
-  console.log("hello");
+  $("input#date").change(function(event){
+    event.preventDefault();
+    // Save the inputted date and remove hyphens
+    const date = $(this).val().split("-");
+    $("#day").val($("#day").val() + convertToDay(date));
+  });
 });
